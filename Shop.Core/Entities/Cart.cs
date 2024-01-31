3aaba40c58a952e91e74282031d16ffba1e32501
Enel <enelry@code.edu.az>
2024-01-31 23:56:40 +0400

    modified cart class

--- Shop.Core/Entities/Cart.cs
+++ Shop.Core/Entities/Cart.cs
@@ -3,7 +3,6 @@
 public class Cart:BaseEntities
 {
     public int Id { get; set; }
-    public int UserId { get; set; }
     public User User { get; set; } = null!;
     public ICollection<CartProduct>? CartProducts { get; set; }
 }
---
+++ Shop.DataAccess/ShopDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Core.Entities;
+
+namespace Shop.DataAccess;
+
+public class ShopDbContext:DbContext
+{
+    ShopDbContext context = new();
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=Shop;Trusted_Connection=true;");
+    }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Wallet> Wallets { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<Invoice> Invoices { get; set; }
+    public DbSet<ProductInvoice> ProductInvoice { get; set; }
+    public DbSet<CartProduct> CartProduct { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Wallets)
+            .WithOne(w => w.User)
+            .HasForeignKey(w => w.UserId);
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Cart)
+            .WithOne(c => c.User)
+            .HasForeignKey<Cart>(c => c.Id);
+
+    }
+}

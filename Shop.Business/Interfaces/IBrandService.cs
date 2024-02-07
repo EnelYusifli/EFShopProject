488e327a58fc8f8613f915b12d8fe57567fe69ff
Enel <enelry@code.edu.az>
2024-02-07 11:39:26 +0400

    modified IBrandService interface

--- Shop.Business/Interfaces/IBrandService.cs
+++ Shop.Business/Interfaces/IBrandService.cs
@@ -1,5 +1,11 @@
-﻿namespace Shop.Business.Interfaces;
+﻿using Shop.Core.Entities;
+
+namespace Shop.Business.Interfaces;
 
 public interface IBrandService
 {
+    void CreateBrand(string name);
+    void UpdateBrand(Brand brand, string name);
+    void DeactivateBrand(int brandId);
+    void ActivateBrand(int brandId);
 }
--- Shop.Business/Interfaces/ICartService.cs
+++ Shop.Business/Interfaces/ICartService.cs
@@ -1,6 +1,9 @@
-﻿namespace Shop.Business.Interfaces;
+﻿using Shop.Core.Entities;
+
+namespace Shop.Business.Interfaces;
 
 public interface ICartService
 {
-
+    void AddProductToCart(int productId, User user, int count = 1);
+    void RemoveProductFromCart(int productId, User user, int count = 1);
 }

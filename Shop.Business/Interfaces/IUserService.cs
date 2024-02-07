cf81e341a89938cabbb214789765bf19b20e5062
Enel <enelry@code.edu.az>
2024-02-07 11:32:20 +0400

    modified IWalletService interface

--- Shop.Business/Interfaces/IUserService.cs
+++ Shop.Business/Interfaces/IUserService.cs
@@ -1,8 +1,20 @@
-﻿namespace Shop.Business.Interfaces;
+﻿using Shop.Core.Entities;
+
+namespace Shop.Business.Interfaces;
 
 public interface IUserService
 {
     void CreateUser(string name, string? surname, int? age, string email, string password, string username, string? phone, string? address);
     void LoginUserWithEmail(string email,string password );
     void LoginUserWithUsername(string username,string password );
+     User FindUserByEmail(string email);
+     User FindUserByUsername(string username);
+     void LoginAdminWithEmail(string email, string password);
+     void LoginAdminWithUsername(string username, string password);
+     void DeactivateUser(int userId);
+     void ActivateUser(int userId);
+     void UpdateUser(User user, string name, string? surname, string email, string username, string password, string? phone, string? address);
+     string HashPassword(string input);
+     string ReadPasswordFromConsole();
+    void GetBoughtProducts(User user);
 }
--- Shop.Business/Interfaces/IWalletService.cs
+++ Shop.Business/Interfaces/IWalletService.cs
@@ -1,5 +1,13 @@
-﻿namespace Shop.Business.Interfaces;
+﻿using Shop.Core.Entities;
+
+namespace Shop.Business.Interfaces;
 
 public interface IWalletService
 {
+    void CreateCard(User user, string cardNumber, string cvc, decimal balance);
+    void DeactivateWallet(int walletId, User user);
+    void ActivateWallet(int walletId, User user);
+    void IncreaseBalance(int walletId, User user, decimal amount);
+    void TransferMoney(int walletIdToIncrease, int walletIdToTransfer, User user, decimal transferAmount);
+    void PayForInvoice(int walletId, User user, decimal amount);
 }
